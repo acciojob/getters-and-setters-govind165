a2a13018c2586cffc9427dd8cd3dbdbdca89b499
govind165 <mrgovindsarbes@gmail.com>
2026-07-14 22:27:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
-//complete this code
 class Person {
-	constructor(Name,Age){
+	constructor(Name, Age) {
 		this._Name = Name;
 		this._Age = Age;
 	}
 
-	get Name(){
-		return this._Name; 
+	get Name() {
+		return this._Name;
 	}
 
-	set Name(newName){
+	set Name(newName) {
 		this._Name = newName;
 	}
 
-		get Age(){
-		return this._Age; 
+	get Age() {
+		return this._Age;
 	}
 
-	set Age(newAge){
+	set Age(newAge) {
 		this._Age = newAge;
 	}
 }
 
 class Student extends Person {
-	constructor(Name,Age){
-		super(Name,Age)
+	constructor(Name, Age) {
+		super(Name, Age);
 	}
 }
 
-class Teacher extends Person {	
-	constructor(Name,Age){
-		super(Name,Age)
+class Teacher extends Person {
+	constructor(Name, Age) {
+		super(Name, Age);
 	}
 }
 
